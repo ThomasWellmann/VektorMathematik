60c18293689a955d9d5334e51ff5296aafc7c624
Thomas Wellmann da Riva Cesar <thomadastube@gmail.com>
2024-06-14 15:05:20 +0200

    Add project files.

---
+++ VektorMathematik/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VektorMathematik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ VektorMathematik/Vector.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VektorMathematik
+{
+    internal class Vector
+    {
+        private int x;
+        private int y;
+        private int z;
+
+        public Vector(int _x = 0, int _y = 0, int _z = 0);
+    }
+}
